0c65e41f7d2c534e98256c73b683323c1058b46f
Gregor Harlan <gharlan@web.de>
2012-11-22 01:44:10 +0100

    use new pages syntax

--- plugins/manager/config.inc.php
+++ plugins/manager/config.inc.php
@@ -1,41 +1,29 @@
 <?php
 ## register paths
-rex_xform::addPath('manager', 'value', rex_path::plugin("xform","manager","lib/value/"));
-rex_xform::addPath('manager', 'action', rex_path::plugin("xform","manager","lib/action/"));
+rex_xform::addPath('manager', 'value', rex_path::plugin('xform', 'manager', 'lib/value/'));
+rex_xform::addPath('manager', 'action', rex_path::plugin('xform', 'manager', 'lib/action/'));
 
-if(rex::isBackend() && is_object(rex::getUser()))
-{
+if (rex::isBackend() && is_object(rex::getUser())) {
   $tables = new rex_xform_manager();
   $tables = $tables->getTables();
-  
-  if(is_array($tables))
-  {
-    foreach($tables as $table)
-    {
+
+  if (is_array($tables) && !empty($tables)) {
+    $pages = array();
+
+    foreach ($tables as $table) {
       ## check addon-permission
-      $table_perm = rex_xform_manager_table::getTablePermName($table["table_name"]);
+      $table_perm = rex_xform_manager_table::getTablePermName($table['table_name']);
       rex_perm::register($table_perm);
-  
+
       ## check active-state and permissions
-      if($table['status'] == 1 && $table['hidden'] != 1 && rex::getUser() && (rex::getUser()->isAdmin() || rex::getUser()->hasPerm($table_perm)))
-      {
-        $be_page = new rex_be_page($table['name'],
-        array(  'page' => 'xform',
-                'subpage' => 'manager',
-                'tripage' => 'data_edit',
-                'table_name' => $table['table_name']
-                )
-        );
-   
-        $be_page->setHref('index.php?page=xform&subpage=manager&tripage=data_edit&table_name='.$table['table_name']);
-        $subpages[] = new rex_be_page_main('tables', $be_page);
+      if ($table['status'] == 1 && $table['hidden'] != 1 && rex::getUser() && (rex::getUser()->isAdmin() || rex::getUser()->hasPerm($table_perm))) {
+        $be_page = new rex_be_page('xform_table_' . $table['table_name'], $table['name']);
+
+        $be_page->setPath($this->getBasePath('pages/data_edit.inc.php'));
+        $pages[] = new rex_be_page_main('tables', $be_page);
       }
     }
-    
-    $this->setProperty('pages', $subpages);
-        
-    ## deactivate xform menu in be_navigation
-    if(rex_request("tripage","string") == "data_edit")
-      $this->getAddon()->setProperty('navigation', array('activateCondition' => array('page' => 'xformmm'),'hidden' => FALSE));
+
+    $this->setProperty('pages', $pages);
   }
 }
--- plugins/manager/pages/data_edit.inc.php
+++ plugins/manager/pages/data_edit.inc.php
@@ -1,19 +1,16 @@
 <?php
 
-$table_name = rex_request("table_name","string");
+$table_name = substr(rex_be_controller::getCurrentPage(), 12);
 
-if(
-	$table_name != "" 
-	&& rex::getUser() 
-	&& (rex::getUser()->isAdmin() || rex::getUser()->hasPerm(rex_xform_manager_table::getTablePermName($table_name)) )
-	)
-{
+if (
+  $table_name != ''
+  && rex::getUser()
+  && (rex::getUser()->isAdmin() || rex::getUser()->hasPerm(rex_xform_manager_table::getTablePermName($table_name)) )
+  ) {
 
-	$page = new rex_xform_manager();
-	$page->setFilterTable($table_name);
-	$page->setLinkVars(	array('page'=>'xform', 'subpage'=>'manager', 'tripage'=>'data_edit', 'table_name'=>$table_name)	);
-	echo $page->getDataPage();
+  $page = new rex_xform_manager();
+  $page->setFilterTable($table_name);
+  $page->setLinkVars(  array('page' => rex_be_controller::getCurrentPage())  );
+  echo $page->getDataPage();
 
 }
-
-?>
